e76712e14f3f3818c044899e1d8529a14ca79749
unknown <gavrilariendra00@gmail.com>
2025-07-09 20:43:01 +0700

    initialization

--- api/controllers/player.go
+++ api/controllers/player.go
@@ -1,11 +1,73 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/database"
+	"api/models"
+	"api/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Register(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request"})
+		return
+	}
 
+	hashedPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	user := models.Player{
+		Username: req.Username,
+		Password: hashedPassword,
+	}
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+	wallet := models.Wallet{
+		PlayerID: user.ID,
+		Balance:  0,
+	}
+	database.DB.Create(&wallet)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User registered successfully",
+	})
 }
 
 func Login(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request"})
+		return
+	}
 
+	var user models.Player
+	if err := database.DB.Where("username = ?", req.Username).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	token, err := utils.GenerateJWT(user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Login successfully",
+		"token":   token,
+	})
 }
--- api/models/wallet.go
+++ api/models/wallet.go
@@ -4,7 +4,7 @@ import "time"
 
 type Wallet struct {
 	ID        uint `gorm:"primaryKey"`
-	UserID    uint `gorm:"uniqueIndex"` // unique = 1 user 1 wallet
+	PlayerID  uint `gorm:"uniqueIndex"` // unique = 1 user 1 wallet
 	Balance   int64
 	CreatedAt time.Time
 	UpdatedAt time.Time
