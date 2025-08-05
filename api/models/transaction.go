efbe72faa83d2ec31355a993bbcd8b5346841760
unknown <gavrilariendra00@gmail.com>
2025-08-05 22:29:21 +0700

    chore(bank) refactoring transaction model excluding player entity

--- api/models/transaction.go
+++ api/models/transaction.go
@@ -5,7 +5,6 @@ import "gorm.io/gorm"
 type Transaction struct {
 	gorm.Model
 	PlayerID    uint   `json:"player_id"`
-	Player      Player `gorm:"foreignKey:PlayerID"`
 	Type        string `json:"type"`
 	Amount      int64  `json:"amount"`
 	Description string `json:"description"`
